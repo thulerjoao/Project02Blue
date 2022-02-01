aa1f78b96070a3380c098244bf9806c4194ba985
thulerjoao <thuler_lima@hotmail.com>
2022-02-01 13:00:41 -0300

    jokenpo code

---
+++ jokenpo.js
@@ -0,0 +1,79 @@
+var prompt = require('prompt-sync')();
+
+
+const random = (num) => Math.random() * num;
+const variaveis = ["pedra","papel","tesoura"];
+let vitoriasJogador; //contador das vezes que o jogador ganhou
+let vitoriasPc; //contador das vezes que a máquina ganhou
+let nRodadas;
+let escolhaJogador;
+let escolhaPc;
+let novamente = 1;
+
+console.log(`*** Jokenpô ***`);
+console.log(`O jogo é simples. A cada rodada, opte por [pedra], [papel] ou [tesoura] e conte com a sorte para vencer seu oponente virtual. \n`);
+
+while(novamente == 'sim' || novamente == 's' || novamente == 1){ 
+    
+    vitoriasJogador = 0;
+    vitoriasPc = 0;
+
+    nRodadas = parseInt(prompt(`Quantas rodadas você quer fazer? `));
+        while(nRodadas < 1){
+            nRodadas = parseInt(prompt(`Digite um número de rodadas maior que 0: `));
+        }
+        console.clear();
+        console.log(`Teremos ${nRodadas} rodadas! `);
+
+    for(i=0; i < nRodadas; i++){
+      escolhaJogador = prompt(`Rodada ${i+1} - Faça sua escolha: `).toLowerCase();    
+      while(escolhaJogador != "pedra" && escolhaJogador != "papel" && escolhaJogador != "tesoura"){
+           escolhaJogador = prompt(`Entrada inválida. Escolha entre [pedra] [papel] [tesoura]:`).toLowerCase();
+          console.log(escolhaJogador);
+       }
+      escolhaPc = variaveis[parseInt(random(3))];
+    
+        if(escolhaJogador == "pedra" && escolhaPc == "tesoura"){
+            vitoriasJogador += 1;
+        }else if(escolhaJogador == "papel" && escolhaPc == "pedra"){
+            vitoriasJogador += 1;
+        } else if(escolhaJogador == "tesoura" && escolhaPc == "papel"){
+            vitoriasJogador += 1;
+        } else if(escolhaJogador == "pedra" && escolhaPc == "papel"){
+            vitoriasPc += 1;
+        } else if(escolhaJogador == "papel" && escolhaPc == "tesoura"){
+            vitoriasPc += 1;
+        } else if(escolhaJogador == "tesoura" && escolhaPc == "pedra"){
+            vitoriasPc += 1;
+        }
+    
+        console.clear();
+        console.log(`Você jogou ${escolhaJogador.toUpperCase()} e o computador ${escolhaPc.toUpperCase()}.`);
+        
+        if(escolhaJogador == escolhaPc){     
+            console.log(`A rodada empatou e o placar continua igual:`);
+        }
+
+        if(vitoriasJogador>vitoriasPc){
+            console.log(`Você VENCE por ${vitoriasJogador} X ${vitoriasPc} \n`);
+        }else if(vitoriasJogador<vitoriasPc){
+            console.log(`Você PERDE por ${vitoriasPc} X ${vitoriasJogador} \n`);
+        }else if(vitoriasJogador == vitoriasPc){console.log(`Vocês EMPATAM por ${vitoriasPc} X ${vitoriasJogador} \n`);
+        }
+    }
+    if(vitoriasJogador > vitoriasPc){
+        console.log(`Parabéns!!! Prove que consegue derrotar o computador mais uma vez!`);
+    }else if(vitoriasJogador == vitoriasPc){
+        console.log(`Empate nunca é o melhor resultado. Tente agora uma vitória!`);
+    }else{
+        console.log(`Uma pena que não tenha vencido. Mas você pode se redimir tentando de novo...`);
+}
+    novamente = prompt(`Deseja jogar novamente? [s] ou [n]: `).toLowerCase();
+    while(novamente != 'sim' && novamente != 's' && novamente != 1 
+            && novamente != 'não' && novamente != 'nao'&& novamente != 'n' && novamente != 0){
+        novamente = prompt(`Entrada inválida. Digite [s] ou [n]`).toLowerCase();
+    }
+       
+}
+
+    
